fa604f0247c1aa67b861255fd2ab8907ddac5e35
RYU9onna <129857611+RYU9onna@users.noreply.github.com>
2023-04-14 21:07:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # ここでChatGPTから情報を取得し、日本語に翻訳するコードを実装します。
-    return render_template('index.html')  # 結果をindex.htmlテンプレートにレンダリングします。
+    return render_template('index.html', translated_info=translated_info)
 
 if __name__ == '__main__':
     app.run(debug=True)
