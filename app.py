2e0904a73b93b3745e8be71db21bd4f6c1d4446d
RYU9onna <129857611+RYU9onna@users.noreply.github.com>
2023-04-14 21:25:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
 from flask import Flask, render_template
 import os
+import tweepy
+import googletrans
+from googletrans import Translator
+import openai
 
 app = Flask(__name__)
 
+def get_twitter_auth():
+    consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
+    consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
+    access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
+    access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    return tweepy.API(auth)
+
+def fetch_tweets(api):
+    # ここでキーワードや検索条件を設定して、Tweetを検索します。
+    search_words = "ChatGPT"
+    tweets = api.search(q=search_words, count=10)
+    return [tweet.text for tweet in tweets]
+
+def translate_text(text):
+    translator = Translator()
+    return translator.translate(text, dest='ja').text
+
+def fetch_gpt_info():
+    api = get_twitter_auth()
+    tweets = fetch_tweets(api)
+    translated_tweets = [translate_text(tweet) for tweet in tweets]
+    return translated_tweets
+
 @app.route('/')
 def index():
-    # ここでChatGPTから情報を取得し、日本語に翻訳するコードを実装します。
+    translated_info = fetch_gpt_info()
     return render_template('index.html', translated_info=translated_info)
 
 if __name__ == '__main__':
