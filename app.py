ed24884c1af3182b816602968c51897a9cf4accf
RYU9onna <129857611+RYU9onna@users.noreply.github.com>
2023-04-14 21:03:07 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # ここでChatGPTから情報を取得し、日本語に翻訳するコードを実装します。
+    return render_template('index.html')  # 結果をindex.htmlテンプレートにレンダリングします。
+
+if __name__ == '__main__':
+    app.run(debug=True)
